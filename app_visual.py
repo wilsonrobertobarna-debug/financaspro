978744242bdded58141fb65c9c547f2c41e67af9
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-05-12 14:59:26 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -386,7 +386,7 @@ with st.sidebar.expander("⚙️ Ajustar Lançamento", expanded=False):
         df_v_display = df_v[['ID', 'Vencimento', 'Tipo', 'Valor', 'Descrição', 'Categoria', 'Banco', 'Status']].copy()
         df_v_display['Valor'] = df_v['V_Num'].apply(m_fmt)
         st.dataframe(df_v_display.iloc[::-1], use_container_width=True, hide_index=True)
-
+        
 elif "Pendências" in aba:
     st.title("📋 Lançamentos Pendentes")
     st.subheader("🔔 Avisos: Vencimentos de Lançamentos")
