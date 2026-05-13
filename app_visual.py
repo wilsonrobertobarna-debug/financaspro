c4418213ee04cc4097194fc20573eb77faff88a1
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-05-13 12:52:28 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -362,7 +362,7 @@ with st.expander("📊 Clique para ver o Comparativo de Sobra Mensal", expanded=
     s_ini = c_d1.date_input("Início", datetime.now() - relativedelta(months=1), format="DD/MM/YYYY")
     s_fim = c_d2.date_input("Fim", datetime.now(), format="DD/MM/YYYY")
         
-        c1, c2, c3 = st.columns(3)
+    c1, c2, c3 = st.columns(3)
         s_bnc = c1.multiselect("Filtrar Banco:", sorted(bancos_disponiveis))
         s_sta = c2.multiselect("Filtrar Status:", ["Pago", "Pendente"])
         b_desc = c3.text_input("Buscar Beneficiário:")
