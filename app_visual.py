4e12694e175dbdd70a3ac11d1780b4fc28471b3e
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-05-17 17:31:12 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -325,7 +325,7 @@ elif "🐶" in aba:
 
 elif "💬" in aba or "📋" in aba:
     st.title("💬 Notificações & Relatórios")
-    st.write("Configurações do sistema FinançasPro.")   
+    st.write("Configurações do sistema FinançasPro.")
     
         # --- RESUMO DOS MESES (DENTRO DO MESMO BLOCO) ---
         with st.expander("📊 RESUMO DOS MESES", expanded=False):
