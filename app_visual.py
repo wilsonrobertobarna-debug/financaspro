58a7ac6ace786fb4d2ded80af989c9b4f9d376e3
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-05-17 16:24:25 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -345,7 +345,7 @@ elif "💬" in aba or "📋" in aba:
             # Agora o m1 vai encontrar o df_m_limpo porque estão no mesmo "quarto"
         m1.metric("📈 Receita", m_fmt(df_m_limpo[df_m_limpo['Tipo'] == 'Receita']['V_Num'].sum()))
             m2.metric("📉 Despesa", m_fmt(df_m_limpo[df_m_limpo['Tipo'] == 'Despesa']['V_Num'].sum()))
-                m3.metric("⚖️ Balanço", m_fmt(saldo_geral))
+            m3.metric("⚖️ Balanço", m_fmt(saldo_geral))
 
         # --- BANCOS E CARTÕES ---
         with st.expander("🏦 BANCOS E CARTÕES", expanded=False):
