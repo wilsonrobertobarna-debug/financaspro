786b4c78d980293e310652ff38353b92196e69a2
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-05-21 10:59:21 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -490,7 +490,6 @@ if "💰" in aba:
         df_v_display['Valor'] = df_v['V_Num'].apply(m_fmt)
         st.dataframe(df_v_display.iloc[::-1], use_container_width=True, hide_index=True)
 
-Python
 elif "Pendências" in aba:
     st.title("📋 Lançamentos Pendentes")
     
