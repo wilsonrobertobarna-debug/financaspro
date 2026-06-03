633e65b79bff36c94bac620e892909a22c5e3747
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-06-03 11:31:57 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -377,11 +377,11 @@ with st.sidebar.expander("⚙️ Ajustar Lançamento", expanded=True):
             id_procurado = st.text_input("Digite o ID que deseja editar:")
 
 if st.button("Buscar ID"):
-             celula = ws_base.find(str(id_procurado).strip(), in_column=9)
-            if celula:
+    celula = ws_base.find(str(id_procurado).strip(), in_column=9)
+    if celula:
         st.session_state['linha_edit'] = celula.row
         st.success(f"ID {id_procurado} encontrado na linha {celula.row}!")
-       else:
+    else:
         st.error("ID não encontrado na coluna I.")
 
 # Só exibe a edição se a linha estiver na memória
