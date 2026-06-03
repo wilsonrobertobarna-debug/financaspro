de8e5f9a3ebcb93ea36bfd22596643e34c3cb9c0
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-06-03 15:30:05 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -513,11 +513,11 @@ if "💰" in aba:
               
       
                
-   if 'df_m_limpo' in locals() and df_m_limpo is not None and not df_m_limpo.empty:
-    st.subheader("🎯 Metas vs Realizado")
-    df_metas_graph = df_m_limpo[df_m_limpo['Tipo'] == 'Despesa'].groupby('Categoria')['V_Num'].sum().reset_index()
+       if 'df_m_limpo' in locals() and df_m_limpo is not None and not df_m_limpo.empty:
+        st.subheader("🎯 Metas vs Realizado")
+        df_metas_graph = df_m_limpo[df_m_limpo['Tipo'] == 'Despesa'].groupby('Categoria')['V_Num'].sum().reset_index()
 
-    if not df_metas_graph.empty:
+        if not df_metas_graph.empty:
         if 'Meta' not in df_metas_graph.columns:
             df_metas_graph['Meta'] = 0.0
         df_metas_graph['Meta'] = df_metas_graph['Categoria'].apply(lambda cat: st.session_state.get(f"m_{cat}", 0.0))
