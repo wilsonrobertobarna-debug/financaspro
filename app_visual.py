4a3bc013c437d9bd9d4cc3bcdc19bdc90d928b56
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-06-19 07:57:14 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -533,7 +533,8 @@ if "💰" in aba:
             
             df_pivot['Variação (%)'] = ((df_pivot[col_atual] - df_pivot[col_anterior]) / df_pivot[col_anterior] * 100).replace([float('inf'), -float('inf')], 0).fillna(0)
         
-        st.dataframe(df_pivot, use_container_width=True)                    
+        df_exibicao = df_pivot.fillna(0)
+        st.table(df_exibicao)                    
         
             # --- AQUI COMEÇA O WILSONBOT ---
         st.subheader("🤖 Consultor WilsonBot")
