20ca51262c3957f1266f6ae994a79857761b1ac9
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-06-23 16:33:06 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -322,7 +322,7 @@ st.sidebar.divider()
 aba = st.session_state.page
 
 # --- PAINEL MESTRE (DOIS EM UM) ---
-if st.session_state.get('page') == 'Home': # Verifique se 'Home' é o nome da sua página principal
+    if st.session_state.get('page') == 'Home': # Verifique se 'Home' é o nome da sua página principal
 with st.expander("📊 Clique aqui para ver o Painel e Relatório Bancário", expanded=False):
         
     # 1. Painel Financeiro
