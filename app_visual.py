2e295a0a81b9c46ad0930339cbe360d0e0fd9207
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-06-24 10:18:29 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -1264,7 +1264,7 @@ if aba == "📋 Relatório PDF":
                 if pd.isna(valor_val): valor_val = 0.0
                 saldo_val = row.get('Saldo_Acum', 0.0)
                 status_val = str(row.get('Status', '-'))
-                ben_val = str(row.get('Beneficiario', '---'))[:20] # Pega até 20 caracteres
+                ben_val = str(row.get('Beneficiário', '---'))[:20] # Pega até 20 caracteres
                 bnc_val = str(row.get('Banco', '---'))[:15]        # Pega até 15 caracteres
 
                 if "DESPESA" in tipo_str.upper() or "GASTO" in tipo_str.upper():
