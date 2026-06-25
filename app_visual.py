2d9d92af4301ffeffd7586b8150f1d528aa150df
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-06-25 09:08:20 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -14,17 +14,17 @@ import streamlit.components.v1 as components
 def get_data():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     
-    # Usamos o caminho completo com o nome correto do arquivo que você encontrou:
-    caminho_json = r"C:\Users\wil_r\OneDrive\Área de Trabalho\FinançasPro\financaspro-wilson-723758e211e3.json"
+    # Use barras normais (/) para evitar conflitos no Windows
+    caminho_json = "C:/Users/wil_r/OneDrive/Área de Trabalho/FinançasPro/financaspro-wilson-723758e211e3.json"
     
     creds = Credentials.from_service_account_file(caminho_json, scopes=scope)
     client = gspread.authorize(creds)
     
-    # ID da sua planilha (confirmado)
+    # ID da planilha
     sheet = client.open_by_key("147vDx908UMco7LByhOZjCGWCOoX8pEyAq-xG2BHaaU4").sheet1 
     data = sheet.get_all_records()
     return pd.DataFrame(data)
-
+    
 @st.cache_data(ttl=600)
 def carregar_dados_do_sheets():
     return get_data()
