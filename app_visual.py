89c443f6574347608a94798b1dc79b0cafb85f5a
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-01 12:18:06 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -1212,11 +1212,11 @@ if aba == "📋 Relatório PDF":
             # REGRA 1: Se for Cartão de Crédito, o saldo inicial DEVE vir zerado
                   # Substitua todo aquele bloco anterior por esta lógica simples:
                     # Cálculo de Saldo (Separando Receitas e Despesas)
-                    hist_anterior = df_rep[df_rep['Vencimento'] < pd.to_datetime(b_ini)]
+            hist_anterior = df_rep[df_rep['Vencimento'] < pd.to_datetime(b_ini)]
                     
-                    receitas = hist_anterior[hist_anterior['Tipo'] != "Despesa"]['Valor'].sum()
-                    despesas = hist_anterior[hist_anterior['Tipo'] == "Despesa"]['Valor'].sum()
-                    saldo_acumulado = receitas - despesas
+            receitas = hist_anterior[hist_anterior['Tipo'] != "Despesa"]['Valor'].sum()
+            despesas = hist_anterior[hist_anterior['Tipo'] == "Despesa"]['Valor'].sum()
+            saldo_acumulado = receitas - despesas
 
                     # 3.2 Agora, calculamos a movimentação que aconteceu desde o começo até o dia 17/05
                     df_historico = df_base.copy()
