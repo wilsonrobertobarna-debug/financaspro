ab6fa9de1349393eab720b16920f50e8a8dccef4
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-01 12:26:58 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -1219,11 +1219,6 @@ if aba == "📋 Relatório PDF":
                     # 3.2 Agora, calculamos a movimentação que aconteceu desde o começo até o dia 17/05
            # Substitua todo aquele bloco anterior por esta lógica simples:
             # Cálculo de Saldo (Separando Receitas e Despesas)
-            hist_anterior = df_rep[df_rep['Vencimento'] < pd.to_datetime(b_ini)]
-            
-            receitas = hist_anterior[hist_anterior['Tipo'] != "Despesa"]['Valor'].sum()
-            despesas = hist_anterior[hist_anterior['Tipo'] == "Despesa"]['Valor'].sum()
-            saldo_acumulado = receitas - despesas
                     
                     if col_data_h:
                         df_historico['DT_HIST'] = pd.to_datetime(df_historico[col_data_h], format="%d/%m/%Y", errors='coerce')
