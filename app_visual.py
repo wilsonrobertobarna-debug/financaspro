37190c8b2e0e2f946115d4f9d5e0d65e13d4b7b5
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-06 10:19:29 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -688,8 +688,8 @@ if "💰" in st.session_state.page:
 elif "Pendências" in aba:
     st.title("📋 Lançamentos Pendentes")
     
-  elif "Pendências" in aba:
-    st.title("📋 Lançamentos Pendentes")
+  #elif "Pendências" in aba:
+    #st.title("📋 Lançamentos Pendentes")
     
     # 1. DEFINIÇÃO DOS FILTROS
     col_a, col_b, col_c = st.columns(3)
