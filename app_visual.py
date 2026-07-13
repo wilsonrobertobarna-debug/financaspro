cdd8b739d830a6d688ae8a90b1c559586af422c3
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-13 15:42:31 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -629,10 +629,7 @@ if "💰" in st.session_state.page:
 
         # Agora o st.dataframe vai encontrar a variável formatacao
         st.dataframe(df_pivot.style.format(formatacao), use_container_width=True)
-        # Aplicamos o estilo (o .style.format aplica o que definimos no dicionário)
-
-        
-        st.dataframe(df_pivot.style.format(formatacao), use_container_width=True)
+        # Aplicamos o estilo (o .style.format aplica o que definimos no dicionário
         
         # --- FILTRO DE ALERTA: PENDÊNCIAS DO MÊS ---
         st.subheader("🔔 Monitor de Pendências do Período")
