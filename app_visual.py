75de8518b3fda3e467faa7f57120c4fb628a30af
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-13 09:30:55 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -632,8 +632,6 @@ if "💰" in st.session_state.page:
         # Aplicamos o estilo (o .style.format aplica o que definimos no dicionário)
 
         
-        st.dataframe(df_pivot.style.format(formatacao), use_container_width=True)
-        
         # --- FILTRO DE ALERTA: PENDÊNCIAS DO MÊS ---
         st.subheader("🔔 Monitor de Pendências do Período")
         
