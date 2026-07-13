bd3cd70af431ee195cdfb4a236bdf81e119ead60
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-13 15:19:13 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -596,7 +596,7 @@ if aba in ["Finanças", "Bancos"]:
     
    
        # --- 1. COMPARATIVO MENSAL ---
-with st.expander("🔄 Ver Comparativo: Mês Anterior vs. Mês Atual"):
+    with st.expander("🔄 Ver Comparativo: Mês Anterior vs. Mês Atual"):
 if 'mes_atual' in locals() and "/" in mes_atual:
         mes_escolhido = int(mes_atual.split('/')[0])
         mes_anterior = mes_escolhido - 1 if mes_escolhido > 1 else 12
