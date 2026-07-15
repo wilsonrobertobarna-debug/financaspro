0a92b9106bb0f5bb8bd23f0767b4da82bbc144a5
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-15 10:39:37 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -902,17 +902,19 @@ elif "📄" in aba:
     # Define o primeiro dia do mês atual e o dia de hoje (ou último dia do mês)
    import calendar
 
-    # 1. Pega o mês e ano de hoje
-        ano = hoje_br.year
-        mes = hoje_br.month
-        
-        # 2. Descobre o último dia do mês atual
-        ultimo_dia = calendar.monthrange(ano, mes)[1]
-        data_fim_mes = hoje_br.replace(day=ultimo_dia)
-        
-        c1, c2 = st.columns(2)
-        d_ini = c1.date_input("Início", hoje_br.replace(day=1), format="DD/MM/YYYY", key="zap_d1")
-        d_fim = c2.date_input("Fim", data_fim_mes, format="DD/MM/YYYY", key="zap_d2")
+   import calendar
+
+# 1. Pega o mês e ano de hoje
+ano = hoje_br.year
+mes = hoje_br.month
+
+# 2. Descobre o último dia do mês atual
+ultimo_dia = calendar.monthrange(ano, mes)[1]
+data_fim_mes = hoje_br.replace(day=ultimo_dia)
+
+c1, c2 = st.columns(2)
+d_ini = c1.date_input("Início", hoje_br.replace(day=1), format="DD/MM/YYYY", key="zap_d1")
+d_fim = c2.date_input("Fim", data_fim_mes, format="DD/MM/YYYY", key="zap_d2")
         
         saldos_txt = ""
         total_patrimonio = 0.0 
