89b9e49eb1a26d3f469da3c19ea29d95e247944e
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-15 11:25:22 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -897,15 +897,15 @@ elif "📄" in aba:
     st.title("📄 WhatsApp")
     
 # --- CORREÇÃO DA INDENTAÇÃO AQUI ---
-        import calendar
-        ano = hoje_br.year
-        mes = hoje_br.month
-        ultimo_dia = calendar.monthrange(ano, mes)[1]
-        data_fim_mes = hoje_br.replace(day=ultimo_dia)
+    import calendar
+    ano = hoje_br.year
+    mes = hoje_br.month
+    ultimo_dia = calendar.monthrange(ano, mes)[1]
+    data_fim_mes = hoje_br.replace(day=ultimo_dia)
         
-        c1, c2 = st.columns(2)
-        d_ini = c1.date_input("Início", hoje_br.replace(day=1), format="DD/MM/YYYY", key="zap_d1")
-        d_fim = c2.date_input("Fim", data_fim_mes, format="DD/MM/YYYY", key="zap_d2")
+    c1, c2 = st.columns(2)
+    d_ini = c1.date_input("Início", hoje_br.replace(day=1), format="DD/MM/YYYY", key="zap_d1")
+    d_fim = c2.date_input("Fim", data_fim_mes, format="DD/MM/YYYY", key="zap_d2")
         # ------------------------------------
     saldos_txt = ""
     total_patrimonio = 0.0 
