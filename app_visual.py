261014f8fa24da1a7d9bbcb50c0c6e7fbf10c44f
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-15 08:43:58 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -704,7 +704,6 @@ if "💰" in st.session_state.page:
         else:
             st.warning("Base de dados vazia.")
 elif "Pendências" in aba:
-elif "Pendências" in aba:
         st.title("📋 Lançamentos Pendentes")
         
         # --- FILTROS UNIFICADOS ---
