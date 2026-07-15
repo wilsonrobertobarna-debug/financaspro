2c38eaea5a8285ea78b1bb21ddad35e4abe946f8
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-15 10:42:39 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -894,21 +894,21 @@ elif "🚗" in aba:
         st.dataframe(df_car_display.iloc[::-1], use_container_width=True, hide_index=True)
 
 elif "📄" in aba:
-    #st.title("📄 WhatsApp")
+    st.title("📄 WhatsApp")
     
     #c1, c2 = st.columns(2)
     #d_ini = c1.date_input("Início", hoje_br - timedelta(days=30), format="DD/MM/YYYY", key="zap_d1")
     #d_fim = c2.date_input("Fim", hoje_br, format="DD/MM/YYYY", key="zap_d2")
     # Define o primeiro dia do mês atual e o dia de hoje (ou último dia do mês)
-       import calendar
+    import calendar
     
-        # 1. Pega o mês e ano de hoje
-        ano = hoje_br.year
-        mes = hoje_br.month
+    # 1. Pega o mês e ano de hoje
+    ano = hoje_br.year
+    mes = hoje_br.month
         
-        # 2. Descobre o último dia do mês atual
-        ultimo_dia = calendar.monthrange(ano, mes)[1]
-        data_fim_mes = hoje_br.replace(day=ultimo_dia)
+    # 2. Descobre o último dia do mês atual
+    ultimo_dia = calendar.monthrange(ano, mes)[1]
+    data_fim_mes = hoje_br.replace(day=ultimo_dia)
         
         c1, c2 = st.columns(2)
         d_ini = c1.date_input("Início", hoje_br.replace(day=1), format="DD/MM/YYYY", key="zap_d1")
