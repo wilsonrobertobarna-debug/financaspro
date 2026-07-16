eb5860e443c3cf605043a8ba2b755d088c53bdbf
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-16 11:36:54 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -978,7 +978,7 @@ elif "📄" in aba:
 
         
             # --- LÓGICA DE CONTA / INVESTIMENTO ---
-        else:
+        
             saldo_inicial = valor_b
             # Para contas normais, mantemos apenas o que já foi 'Pago'
             mov_paga = df_base[(df_base['Banco'] == b) & (df_base['Status'].str.upper() == 'PAGO')]
