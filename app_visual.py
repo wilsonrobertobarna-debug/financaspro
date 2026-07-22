8ecc68304dbdcf99445c3380055344273ccf713b
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-22 15:41:15 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -22,7 +22,7 @@ st.markdown("""
         left: 0;
         width: 100%;
         background-color: #f8f9fa;
-        padding: 10px;
+        padding: 100px;
         z-index: 999;
         border-bottom: 1px solid #ddd;
     }
