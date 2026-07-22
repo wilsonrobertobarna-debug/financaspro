30a82eacac69868b94cb3f2366798982d0acf7aa
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-22 19:51:14 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -10,26 +10,7 @@ from fpdf import FPDF
 import urllib.parse
 import streamlit.components.v1 as components
 
-# --- Tela de login ---
-if 'login' not in st.session_state:
-    st.session_state.login = False
 
-if not st.session_state.login:
-    col1, col_centro, col2 = st.columns([1, 2, 1])
-    with col_centro:
-        st.markdown("<br><br><br>", unsafe_allow_html=True)
-        st.markdown("### 🔒 Acesso Seguro")
-        senha = st.text_input("Digite sua senha:", type="password")
-        if st.button("🔓 Desbloquear Sistema"):
-            if senha == "Wilson123":
-                st.session_state.login = True
-                st.rerun()
-            else:
-                st.error("Senha incorreta, Wilson!")
-    st.stop()
-
-# --- Se login foi feito, mostra barra de navegação ---
-else:
     st.markdown("""
         <style>
         .top-bar {
