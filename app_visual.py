5d7a19f0a00707c7b0171eedf741ab6f56ab81b9
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-07-22 15:43:15 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -22,7 +22,7 @@ st.markdown("""
         left: 0;
         width: 100%;
         background-color: #f8f9fa;
-        padding: 30px;
+        padding: 40px;
         z-index: 999;
         border-bottom: 1px solid #ddd;
     }
