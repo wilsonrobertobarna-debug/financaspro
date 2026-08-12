95b50d8715effb39c2432ebab04150df5a5f3fc5
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-08-12 15:35:04 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -1550,8 +1550,6 @@ elif "🚗" in aba:
 
 elif "📄" in aba:
     st.title("📄 Relatório WhatsApp")
-elif "📄" in aba:
-    st.title("📄 Relatório WhatsApp")
     
     # Trava a data de início no primeiro dia do mês atual
     primeiro_dia_mes = hoje_br.replace(day=1)
