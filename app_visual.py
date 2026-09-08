0ad2a086f3db3baadcc22f8343ad90b90aa70d3f
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-09-08 09:05:08 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -846,12 +846,12 @@ with st.sidebar.expander("🚀 Novo Lançamento", expanded=st.session_state.expa
         
         # Botão de WhatsApp
         if st.button("💬 Enviar Aviso via WhatsApp", key="btn_whats"):
-           # Puxa os dados organizados na seção [twilio] do painel do Streamlit
-            account_sid = st.secrets["twilio"]["account_sid"]
-            auth_token = st.secrets["twilio"]["auth_token"]
-            whatsapp_remetente = st.secrets["twilio"]["whatsapp_from"]
-            whatsapp_destino = st.secrets["twilio"]["whatsapp_to"]
-            
+          
+            # Puxa os dados direto do painel de segredos
+            account_sid = st.secrets["TWILIO_ACCOUNT_SID"]
+            auth_token = st.secrets["TWILIO_AUTH_TOKEN"]
+            whatsapp_remetente = "whatsapp:+19994894920"
+            whatsapp_destino = "whatsapp:+5519999013540"        
             try:
                 df_atual = carregar_dados_gs()
                 
