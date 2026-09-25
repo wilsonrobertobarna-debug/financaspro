aea388cb0895d8a273ebeff374c27463e5769b0b
wilsonrobertobarna-debug <wilsonrobertobarna@gmail.com>
2026-09-25 19:28:32 -0300

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -718,7 +718,7 @@ with st.sidebar.expander("🚀 Novo Lançamento", expanded=st.session_state.expa
     # Um respiro antes de entrar no formulário principal
     st.markdown("")
 
-   # 2. Agora entra o formulário com o restante dos campos e o botão Salvar
+    # 2. Agora entra o formulário com o restante dos campos e o botão Salvar
         with st.form("f_novo"):
             f_val = st.number_input("Valor", min_value=-100000.0, value=0.0, step=0.01, format="%.2f", key="val_novo_lancamento")
             f_par = st.number_input("Parcelas", min_value=1, value=1, key="par_novo_lancamento")
